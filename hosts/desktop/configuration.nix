a904cc579d6c3b7e666bb99f34b1d152211b5b40
mike <michael.e.kirl@gmail.com>
2026-01-17 14:32:48 -0600

    desktop configuration added

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "desktop";
+  networking.networkmanager.enable = true;
+
+  # Timezone
+  time.timeZone = "America/Chicago";
+
+  # NVIDIA
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+  hardware.graphics.enable = true;
+
+  # User
+  users.users.mike = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ];
+    shell = pkgs.fish;
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    wget
+  ];
+
+  # Fish shell
+  programs.fish.enable = true;
+
+  # SSH
+  services.openssh.enable = true;
+
+  # Tailscale
+  services.tailscale.enable = true;
+
+  # Allow unfree (NVIDIA)
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "25.11";
+}
