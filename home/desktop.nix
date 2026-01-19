e28d6d6aac9cd6f44d6b26ea897c7abf6dfbdc1e
mike <mkirl@users.noreply.github.com>
2026-01-19 15:47:45 -0600

    add stdout stderr redirects from command line launcher

--- home/desktop.nix
+++ home/desktop.nix
@@ -12,14 +12,17 @@
     pkgs.firefox
     pkgs.zed-editor
     pkgs.jetbrains.idea
-
     
-    xdg.desktopEntries.idea = {
-      name = "IntelliJ IDEA";
-      exec = "idea -Dawt.toolkit.name=WLToolkit %u";
-      icon = "idea";
-      terminal = false;
-      categories = [ "Development" "IDE" ];
-    };
   ];
+  xdg.desktopEntries.idea = {
+    name = "IntelliJ IDEA";
+    exec = "idea -Dawt.toolkit.name=WLToolkit %u";
+    icon = "idea";
+    terminal = false;
+    categories = [ "Development" "IDE" ];
+  };
+
+  programs.fish.shellAliases = {
+    idea = "idea -Dawt.toolkit.name=WLToolkit &>/dev/null &; disown";
+  };
 }
