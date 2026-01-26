12bce7efee4b8ec28724fdede766deb20b5f3f31
mike <mkirl@users.noreply.github.com>
2026-01-26 14:15:46 -0600

    consolidate configuration

--- home/thinkpad.nix
+++ home/thinkpad.nix
@@ -10,15 +10,5 @@
     ghostty.packages.x86_64-linux.default
     pkgs.vscode
     pkgs.firefox
-    pkgs.zed-editor
-    pkgs.jetbrains.idea-ultimate
   ];
-
-  xdg.desktopEntries.idea = {
-    name = "Intellij IDEA";
-    exec = "idea -Dawt.toolkit.name=WLToolkit %u";
-    icon = "idea";
-    terminal = false;
-    categories = [ "Development" "IDE" ];
-  };
 }
