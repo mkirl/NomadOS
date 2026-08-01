96edf66edbbfd81aaf4bbd5a6af8f6bf4e3001e0
Mike <mkirl@users.noreply.github.com>
2026-08-01 17:25:12 -0500

    add home-manager backup extension to fix file clobber errors

--- modules/features/home-manager.nix
+++ modules/features/home-manager.nix
@@ -3,6 +3,7 @@
     imports = [ inputs.home-manager.nixosModules.home-manager ];
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
+    home-manager.backupFileExtension = "hm-backup";
     home-manager.sharedModules = [ inputs.noctalia.homeModules.default ];
   };
 }
